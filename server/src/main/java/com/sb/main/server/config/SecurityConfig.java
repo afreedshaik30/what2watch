57b6589437b8b9441ada6ebba897f1ce1f05173e
SHAIK AFREED <skafrid482@gmail.com>
2025-07-23 16:13:01 +0530

    env(key,value)

--- server/src/main/java/com/sb/main/server/config/SecurityConfig.java
+++ server/src/main/java/com/sb/main/server/config/SecurityConfig.java
@@ -31,6 +31,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                         .requestMatchers("/api/auth/**").permitAll()
+                        .requestMatchers("/api").permitAll()
                         .anyRequest().authenticated()
                 )
                 .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
